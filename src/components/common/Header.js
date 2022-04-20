2d4e1ae7b89e174d0e3bf4279ce277ef7ec53cc3
eneyeitech <abdulmumineabdulkarim@gmail.com>
2022-04-20 13:59:35 -0700

    React Router implemented

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,9 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+    const activeStyle = {color:"orange"};
     return (
         <nav>
-            <NavLink to="/">Home</NavLink> | <NavLink href="/courses">Courses</NavLink> | <NavLink href="/about">About</NavLink>
+            <NavLink activeStyle={activeStyle} exact to="/">Home</NavLink>
+            {" | "}
+            <NavLink activeStyle={activeStyle} to="/courses">Courses</NavLink>
+            {" | "}
+            <NavLink activeStyle={activeStyle} to="/about">About</NavLink>
         </nav>
     );
 }
