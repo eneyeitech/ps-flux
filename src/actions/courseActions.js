48c92aa18f4c1cd1b54974eecd0e5def57a90338
eneyeitech <abdulmumineabdulkarim@gmail.com>
2022-04-22 10:43:58 -0700

    Flux demos

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -6,8 +6,28 @@ export function saveCourse(course) { // Action Creator
     return courseApi.saveCourse(course).then( savedCourse => {
         // Hey dispatcher, go tell all the stores that a course was just created.
         dispatcher.dispatch({ // Action
-            actionType: actionTypes.CREATE_COURSE,
+            actionType: course.id 
+            ? actionTypes.CREATE_COURSE 
+            : actionTypes.CREATE_COURSE,
             course: savedCourse
+        });
+    });
+}
+
+export function loadCourses() {
+    return courseApi.getCourses().then( courses => {
+        dispatcher.dispatch({ 
+            actionType: actionTypes.LOAD_COURSES,
+            courses: courses
+        });
+    });
+}
+
+export function deleteCourse(id) {
+    return courseApi.deleteCourse(id).then( () => {
+        dispatcher.dispatch({ 
+            actionType: actionTypes.DELETE_COURSE,
+            id: id
         });
     });
 }
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -5,9 +5,8 @@ import { toast } from "react-toastify";
 import * as courseActions from '../actions/courseActions';
 
 const ManageCoursePage = props => {
-
     const [errors, setErrors] = useState({});
-
+    const [courses, setCourses] = useState(courseStore.getCourses());
     const [course, setCourse] = useState({
         id:null,
         slug:"",
@@ -17,11 +16,19 @@ const ManageCoursePage = props => {
     });
 
     useEffect(() => {
+        courseStore.addChangeListener(onChange);
         const slug = props.match.params.slug // from the path `/course/:slug
-        if (slug) {
+        if (courses.length === 0) {
+            courseActions.loadCourses();
+        } else if (slug) {
             setCourse(courseStore.getCourseBySlug(slug));
         }
-    }, [props.match.params.slug]);
+        return () => courseStore.removeChangeListener(onChange);
+    }, [courses.length, props.match.params.slug]);
+
+    function onChange() {
+        setCourses(courseStore.getCourses());
+    }
 
     function formIsValid() {
         const _errors = {};
